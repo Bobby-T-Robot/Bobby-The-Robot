94750c8d0398fd286e75108a2e40cfbf4adca7bb
Bobby-T-Robot <egearcildursun5308@gmail.com>
2024-09-20 21:29:18 +0300

    username update

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ const fadeIn = document.querySelector('.fade-in');
 const fadeIn2 = document.querySelector('.fade-in2');
 const abil = document.querySelector('.abil');
 const abil2 = document.querySelector('.abil2');
+const useranim = document.getElementById('username');
+const userField = document.getElementById('username');
+const nameanim = document.getElementById('nameHere');
+const nameHere = document.getElementById('nameHere');
+const thanks = document.getElementById('thanks');
+const yourName = document.getElementById('yourName');
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  fakeBut2.disabled = true;
+});
+
+setTimeout(() =>{
+  nameanim.classList.add('animate');
+}, 1800);
 
 setTimeout(() =>{
 abil.classList.add('animate');
 }, 3000);
+
+setTimeout(() =>{
+  useranim.classList.add('animate');
+  }, 1900);
 
 setTimeout(() =>{
     abil2.classList.add('animate');
@@ -55,3 +74,47 @@ but3.addEventListener('click', function tabClose() {
   menuRight.classList.toggle('moved');
   console.log('Exited');// oooo kmssss
 });
+
+userField.addEventListener('input', () => {
+  if (userField.value !== '') {
+    fakeBut2.disabled = false;
+  } else {
+    fakeBut2.disabled = true;
+  }
+});
+
+userField.addEventListener("keydown", function(typed){
+  if(typed.keyCode === 13 && userField.value === "") {
+    typed.preventDefault();
+  } else if (typed.keyCode === 13) {
+    alert('Entered ${username}')
+  userField.classList.add("fade-out");
+  nameHere.classList.add("fade-out");
+  thanks.classList.add("notfade-in3");
+
+  setTimeout(function() {
+    userField.remove();
+    userField.disabled = true;
+  nameHere.remove();
+    nameHere.disabled = true;
+  }, 1000);
+  }
+});
+
+document.body.appendChild(yourName);
+
+userField.addEventListener('input', () => {
+  const userName = userField.value;
+  yourName.textContent = `Hello, ${userName}!`;
+});
+
+menuRight.appendChild(yourName);
+
+yourName.style.fontFamily = 'Digital, sans-serif';
+yourName.style.fontSize = '1em';
+yourName.style.color = 'rgba(133, 255, 253, 0.9)'
+yourName.style.letterSpacing = '0.3em';
+yourName.style.textShadow = '0 0 20px';
+yourName.style.position = 'absolute';
+yourName.style.bottom = '87.8%';
+yourName.style.left = '5%';
